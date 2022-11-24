57ecaeaf6f5a9d7d3e8b699d20221507b67ee0f0
Timo Frionnet <tfrionnet@gmail.com>
2022-11-24 13:24:42 +0100

    Updated app to pwa

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { GameScreen } from "../components/GameScreen";
 import { ScoreView } from "../components/ScoreView";
 import { StartScreen } from "../components/StartScreen";
 import { getEmptyScores } from "../Funcs/global";
-import { Player, Round } from "../Types/global";
+import { Player } from "../Types/global";
 
 const Home: NextPage = () => {
     const [gameStarted, setGameStarted] = useState<boolean>(false);
@@ -21,7 +21,15 @@ const Home: NextPage = () => {
             <Head>
                 <title>Nines Score</title>
                 <meta name="description" content="Keep track of your nines score" />
-                <link rel="icon" href="/favicon.ico" />
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
             </Head>
             <main className="flex flex-col justify-center h-screen gap-2 p-4 md:mx-auto md:max-w-md">
