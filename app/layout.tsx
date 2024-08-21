c1ccd860c1ccd094a1d54da000a7481e3709efb4
Timo Frionnet <tfrionnet@gmail.com>
2024-08-21 16:33:28 +0200

    Fixed viewport issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
+import type { Viewport } from "next";
 
 export const metadata: Metadata = {
     title: "Nines Score",
     description: "Keep track of your nines score",
-    viewport: "width=device-width, initial-scale=1, maximum-scale=1",
     icons: {
         apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
         icon: [
@@ -16,10 +16,16 @@ export const metadata: Metadata = {
             { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5" },
         ],
     },
-    themeColor: "#ffffff",
     other: {
         "msapplication-TileColor": "#da532c",
     },
+};
+
+export const viewport: Viewport = {
+    themeColor: "black",
+    initialScale: 1,
+    maximumScale: 1,
+    width: "device-width",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
