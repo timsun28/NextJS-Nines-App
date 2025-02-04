5e770a03e0596f561debd1b8a034e73100775927
Timo Frionnet <tfrionnet@gmail.com>
2025-02-04 10:50:15 +0100

    removed daisyui from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
-    },
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: true,
     },
 };
